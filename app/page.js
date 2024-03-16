2ae5e501e819367e5bb6880363221b7b373e655f
Adebayo Olowofoyeku <adebayoolowofoyeku@gmail.com>
2024-03-16 23:09:23 +0000

    editable content

--- app/page.js
+++ app/page.js
@@ -1,7 +1,39 @@
+"use client"
 import Image from "next/image";
 import styles from "./page.module.css";
+import { createClient } from "@sanity/client";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+
+  const [sections, setSections] = useState([])
+  const [headerSection, setHeaderSection] = useState()
+  const [serviceSection, setServiceSection] = useState()
+  const [aboutSection, setAboutSection] = useState()
+
+  const client = createClient({
+    projectId: 'smqf21u1',
+    dataset: 'production',
+    useCdn: false,
+    apiVersion: '2023-05-03'
+  })
+
+  async function getHomeSections() {
+    const sections = await client.fetch('*[_type == "home"]')
+    console.log(sections)
+    setSections(sections)
+    const header = sections.find(x => x.Section == 'Header')
+    const services = sections.find(x => x.Section == 'OUR SERVICES')
+    const about = sections.find(x => x.Section == 'ABOUT US')
+    setHeaderSection(header)
+    setServiceSection(services)
+    setAboutSection(about)
+    return sections
+  }
+
+  useEffect(() =>{
+    getHomeSections()
+  }, [])
   return (
     <main className={styles.main}>
       <div className={styles.nav}>
@@ -31,10 +63,13 @@ export default function Home() {
       <div className={styles.header}>
         <div className={styles.leftHeader}>
           <div className={styles.headerContent}>
-            <h3>Secure Today, Thrive Tomorrow</h3>
-            <p>Elevate Your Safety with Tsur Business Solutions! Experience peace of mind with our unrivaled solutions. Contact us now for a safer, smarter, and more efficient tomorrow!</p>
+            <h3>{headerSection?.Section_Title}</h3>
+            {/* <h3>Secure Today, Thrive Tomorrow</h3> */}
+            <p>{headerSection?.Section_Description}</p>
+            {/* <p>Elevate Your Safety with Tsur Business Solutions! Experience peace of mind with our unrivaled solutions. Contact us now for a safer, smarter, and more efficient tomorrow!</p> */}
             <div className={styles.headerButton}>
-              <p>Get In Touch</p>
+              <p>{headerSection?.Section_Button}</p>
+              {/* <p>Get In Touch</p> */}
             </div>
           </div>
         </div>
@@ -47,10 +82,12 @@ export default function Home() {
       <div className={styles.services}>
         <div className={styles.sectionHead}>
           <div></div>
-          <p>OUR SERVICES</p>
+          <p>{serviceSection?.Section}</p>
+          {/* <p>OUR SERVICES</p> */}
         </div>
         <div className={styles.sectionSubtitle}>
-          <p>Enhance Your Environment: Unmatched Fire, Security, and Facility Management Solutions</p>
+          <p>{serviceSection?.Section_Title}</p>
+          {/* <p>Enhance Your Environment: Unmatched Fire, Security, and Facility Management Solutions</p> */}
         </div>
         <div className={styles.carousel}>
           <div className={styles.carouselCont}>
@@ -73,13 +110,17 @@ export default function Home() {
         <div className={styles.leftAbout}>
         <div className={styles.sectionHead}>
           <div></div>
-          <p>ABOUT US</p>
+          <p>{aboutSection?.Section}</p>
+          {/* <p>ABOUT US</p> */}
         </div>
         <div className={styles.aboutContent}>
-          <h3>Transformative Fire, Security, and Facility Management Solutions</h3>
-          <p>With a relentless pursuit of excellence, we have positioned ourselves as industry pioneers, offering tailored solutions to meet the unique needs of our clients. Our team is fueled by a passion for safety, efficiency, and unparalleled customer satisfaction</p>
+          <h3>{aboutSection?.Section_Title}</h3>
+          {/* <h3>Transformative Fire, Security, and Facility Management Solutions</h3> */}
+          <p>{aboutSection?.Section_Description}</p>
+          {/* <p>With a relentless pursuit of excellence, we have positioned ourselves as industry pioneers, offering tailored solutions to meet the unique needs of our clients. Our team is fueled by a passion for safety, efficiency, and unparalleled customer satisfaction</p> */}
           <div className={styles.aboutButton}>
-            <p>Talk To Us</p>
+            <p>{aboutSection?.Section_Button}</p>
+            {/* <p>Talk To Us</p> */}
           </div>
         </div>
         </div>
